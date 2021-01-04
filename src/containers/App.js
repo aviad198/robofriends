9a29fb44fdb6b287ed67dce6e3f11fb5baa9f887
aviad198 <aviad198@gmail.com>
2021-01-04 11:08:57 +0200

    Added Error Boundary and deploy app to git

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from "../Components/CardList";
 import SearchBox from '../Components/SearchBox'
 import './App.css'
 import Scroll from '../Components/Scroll';
+import ErrorBoundry from '../Components/ErrorBoundry';
 
 class App extends React.Component {
     constructor() {
@@ -36,7 +37,9 @@ class App extends React.Component {
                     <h1 className={'f1'}>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
-                        <CardList robots={filteredRobots}/>
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots}/>
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
             );
